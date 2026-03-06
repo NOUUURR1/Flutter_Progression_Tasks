22fb69f1e14bc99d945c6580d93917564b2f5cc8
NOUUURR1 <enour7987@gmail.com>
2026-03-06 23:02:56 +0200

    Create folders

---
+++ tasks/simple_calculator_app/lib/features/calculator/presentation/screen/simple_calculator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SimpleCalculator extends StatelessWidget {
+  const SimpleCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
